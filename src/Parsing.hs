41a386af31e52c28f76f3c417b40d9861156ee0a
Oskar Ek <oskarek24@gmail.com>
2018-12-15 17:55:32 +0100

    Parsing.int allows negative numbers

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -3,9 +3,13 @@ module Parsing where
 import qualified Text.Parsec                   as P
 import qualified Text.Parsec.String            as PS
 import qualified Types                         as T
+import Data.Functor ((<$))
+
+modif :: PS.Parser (Int -> Int)
+modif = negate <$ P.char '-' P.<|> pure id
 
 int :: PS.Parser Int
-int = read <$> P.many1 P.digit <* P.spaces
+int = modif <*> (read <$> P.many1 P.digit <* P.spaces)
 
 parse :: PS.Parser a -> String -> T.ParseRes a
 parse p = P.parse p ""
